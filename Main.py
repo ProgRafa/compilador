7b239875e8478a1f5bdcadb53652c07434228f46
functionoliveira <functionoliveira@gmail.com>
2019-11-17 19:59:20 -0300

    Construção da tabela de símbolos.

--- Main.py
+++ Main.py
@@ -1,90 +1,100 @@
 from Reader import *
 from AFD import *
-from SymbolTable import *
+from AFDPatterns import *
 from Token import *
+from SymbolTable import SymbleTableRow, SymbolTable
+
 import sys
 import copy
 
+row = SymbleTableRow('identifier', 'SIDENTIFIER', 'GLOBAL', 'INTEGER')
+table = SymbolTable()
 
-class Compiler:
-    def __init__(self, archive):
-        self.reader = Reader(32, archive)
-        self.row = 1
-        self.col = 1
-        self.tokens = []     
-        self.word = ''
+table.add(row)
+table.toString()
+table.set_attribute('label', 'class', row.id)
+table.toString()
 
-    # Monta os AFDs a partir da tabela de símbolos da linguagem
-    def init_afds(self):
-        self.lexers = []
-        for sym in TABLE:
-            lex = list(sym.keys())[0]
-            p_type = list(sym.values())[0]
-            self.lexers.append((Token(p_type, lex, 0, 0), AFD(sym["AFD"])))
 
-    # Percorre os AFDs para validar uma entrada
-    def find_token(self, p_word):
-        c_row = copy.copy(self.row)
-        c_col = copy.copy(self.col)
-        token = Token('#ERROR', p_word, c_row, c_col)
-        for lex in self.lexers:
-            if lex[1].testing(list(p_word)):
-                token = lex[0]
-                token.lex = p_word
-                token.row = c_row
-                token.colunm = c_col
-                break
-        return token
+# class Compiler:
+#     def __init__(self, archive):
+#         self.reader = Reader(32, archive)
+#         self.row = 1
+#         self.col = 1
+#         self.tokens = []     
+#         self.word = ''
 
-    def parse(self):
-        while self.reader.end_of_archive():
-            # pede um caracter do buffer
-            char = self.reader.pull_buffer()
-            # remove espaços em branco mas conta como coluna
-            if char == ' ':
-                self.col += 1
-                continue
-            # identifica o tab e conta como 4 colunas
-            if char == '\t':
-                self.col += 4
-                continue
-            # identifica quebra de linha, acrescenta a linha e zera a coluna
-            if char == '\n':
-                self.row += 1
-                self.col = 1
-                continue
-            # testa os tokens usando o carcter char que veio do buffer
-            test_char = self.find_token(char)
+#     # Monta os AFDs a partir da tabela de símbolos da linguagem
+#     def init_afds(self):
+#         self.lexers = []
+#         for sym in TABLE:
+#             lex = list(sym.keys())[0]
+#             p_type = list(sym.values())[0]
+#             self.lexers.append((Token(p_type, lex, 0, 0), AFD(sym["AFD"])))
 
-            # identifica que o char é um token, grava a word anterior ao char como identificador
-            if test_char.type != '#ERROR' and test_char.type != '#IDENTIFIER':
-                if self.word != '':
-                    self.tokens.append(copy.copy(self.find_token(self.word)))
-                    self.col += len(self.word)
-                    self.word = ''
-            self.word += char
-            test_word = self.find_token(self.word)
+#     # Percorre os AFDs para validar uma entrada
+#     def find_token(self, p_word):
+#         c_row = copy.copy(self.row)
+#         c_col = copy.copy(self.col)
+#         token = Token('#ERROR', p_word, c_row, c_col)
+#         for lex in self.lexers:
+#             if lex[1].testing(list(p_word)):
+#                 token = lex[0]
+#                 token.lex = p_word
+#                 token.row = c_row
+#                 token.colunm = c_col
+#                 break
+#         return token
+
+#     def parse(self):
+#         while self.reader.end_of_archive():
+#             # pede um caracter do buffer
+#             char = self.reader.pull_buffer()
+#             # remove espaços em branco mas conta como coluna
+#             if char == ' ':
+#                 self.col += 1
+#                 continue
+#             # identifica o tab e conta como 4 colunas
+#             if char == '\t':
+#                 self.col += 4
+#                 continue
+#             # identifica quebra de linha, acrescenta a linha e zera a coluna
+#             if char == '\n':
+#                 self.row += 1
+#                 self.col = 1
+#                 continue
+#             # testa os tokens usando o carcter char que veio do buffer
+#             test_char = self.find_token(char)
+
+#             # identifica que o char é um token, grava a word anterior ao char como identificador
+#             if test_char.type != '#ERROR' and test_char.type != '#IDENTIFIER':
+#                 if self.word != '':
+#                     self.tokens.append(copy.copy(self.find_token(self.word)))
+#                     self.col += len(self.word)
+#                     self.word = ''
+#             self.word += char
+#             test_word = self.find_token(self.word)
             
-            if test_word.type != '#ERROR' and test_word.type != '#IDENTIFIER':
-                self.tokens.append(copy.copy(test_word))
-                self.col += len(self.word)
-                self.word = ''
+#             if test_word.type != '#ERROR' and test_word.type != '#IDENTIFIER':
+#                 self.tokens.append(copy.copy(test_word))
+#                 self.col += len(self.word)
+#                 self.word = ''
             
-# remover o nome do arquivo dos argumentos        
-sys.argv.pop(0)
-arguments = sys.argv
-archive = './archives/'
-if len(arguments) > 0:
-    archive += arguments[0]
-else:
-    archive += 'BinarySearch.java'
+# # remover o nome do arquivo dos argumentos        
+# sys.argv.pop(0)
+# arguments = sys.argv
+# archive = './archives/'
+# if len(arguments) > 0:
+#     archive += arguments[0]
+# else:
+#     archive += 'BinarySearch.java'
 
-print("Analisando o programa", archive)
-compile = Compiler(archive)
-compile.init_afds()
-compile.parse()
+# print("Analisando o programa", archive)
+# compile = Compiler(archive)
+# compile.init_afds()
+# compile.parse()
 
-for token in compile.tokens:
-    print('###################################################')
-    token.to_string()
-    print('\n\n')
+# for token in compile.tokens:
+#     print('###################################################')
+#     token.to_string()
+#     print('\n\n')
--- SymbolTable.py
+++ SymbolTable.py
@@ -1,479 +1,52 @@
+import random
+import copy
 
-TABLE = [
-    # Lexema : Símbolo, Automato : {estados, gramatica, regras}
-    {
-        "class" : "#CLASS", 
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F'],
-            "grammar" : ['c', 'l', 'a', 's'],
-            "rules" : {
-                "A" : { "c" : "B" },
-                "B" : { "l" : "C" },
-                "C" : { "a" : "D" },
-                "D" : { "s" : "E" },
-                "E" : { "s" : "F" },
-                "F" : { " " : "F" }
-            }
-        },
-    },
-    {
-        "{" : "#START",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['{'],
-            "rules" : {
-                "A" : { "{" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "}" : "#END",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['}'],
-            "rules" : {
-                "A" : { "}" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
+class SymbolTable():
+    def __init__(self,):
+        self.table = {}
+        
+    def add(self, row):
+        if row.id in self.table:
+            self.table[row.id] = row
+        else:
+            self.table[row.id] = row
 
-    {
-        "[" : "#ARRAYOPEN",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['['],
-            "rules" : {
-                "A" : { "[" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "]" : "#ARRAYCLOSE",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : [']'],
-            "rules" : {
-                "A" : { "]" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "." : "#POINT",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['.'],
-            "rules" : {
-                "A" : { "." : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "(" : "#PARENTHESISOPEN",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['('],
-            "rules" : {
-                "A" : { "(" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        ")" : "#PARENTHESISCLOSE",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : [')'],
-            "rules" : {
-                "A" : { ")" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "<" : "#MINOR",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['<'],
-            "rules" : {
-                "A" : { "<" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        ">" : "#BIGGER",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['>'],
-            "rules" : {
-                "A" : { ">" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "=" : "#EQUALS",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['='],
-            "rules" : {
-                "A" : { "=" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "!" : "#NOT",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['!'],
-            "rules" : {
-                "A" : { "!" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        ";" : "#STRUCTIONEND",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : [';'],
-            "rules" : {
-                "A" : { ";" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "," : "#COMMA",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : [','],
-            "rules" : {
-                "A" : { "," : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "-" : "#SUBTRACT",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['-'],
-            "rules" : {
-                "A" : { "-" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "*" : "#MULTIPLY",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['*'],
-            "rules" : {
-                "A" : { "*" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "+" : "#SUM",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['+'],
-            "rules" : {
-                "A" : { "+" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
-    {
-        "/" : "#DIVISION",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['/'],
-            "rules" : {
-                "A" : { "/" : "B" },
-                "B" : { " " : "B" }
-            }
-        },
-    },
+    def delete(self, id):
+        if id in self.table:
+            removed = copy.copy(self.table[id])
+            self.table[id] = None
+            del self.table[id]
+            return removed
 
-    {
-        "public" : "#PUBLIC",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            "grammar" : ['p', 'u', 'b', 'l', 'i', 'c'],
-            "rules" : {
-                "A" : { "p" : "B" },
-                "B" : { "u" : "C" },
-                "C" : { "b" : "D" },
-                "D" : { "l" : "E" },
-                "E" : { "i" : "F" },
-                "F" : { "c" : "G" },
-                "G" : { " " : "G" }
-            }
-        },
-    },
-    {
-        "static" : "#STATIC",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            "grammar" : ['s', 't', 'a', 't', 'i', 'c'],
-            "rules" : {
-                "A" : { "s" : "B" },
-                "B" : { "t" : "C" },
-                "C" : { "a" : "D" },
-                "D" : { "t" : "E" },
-                "E" : { "i" : "F" },
-                "F" : { "c" : "G" },
-                "G" : { " " : "G" }
-            }
-        },  
-    },
-    {
-        "void" : "#VOID",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E'],
-            "grammar" : ['v', 'o', 'i', 'd'],
-            "rules" : {
-                "A" : { "v" : "B" },
-                "B" : { "o" : "C" },
-                "C" : { "i" : "D" },
-                "D" : { "d" : "E" },
-                "E" : { " " : "E" },
-            }
-        },
-    },
-    {
-        "String" : "#STRING",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            "grammar" : ['S', 't', 'r', 'i', 'n', 'g'],
-            "rules" : {
-                "A" : { "S" : "B" },
-                "B" : { "t" : "C" },
-                "C" : { "r" : "D" },
-                "D" : { "i" : "E" },
-                "E" : { "n" : "F" },
-                "F" : { "g" : "G" },
-                "G" : { " " : "G" },
-            }
-        },
-    },
-    {
-        "int" : "#INTEGER",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D'],
-            "grammar" : ['i', 'n', 't'],
-            "rules" : {
-                "A" : { "i" : "B" },
-                "B" : { "n" : "C" },
-                "C" : { "t" : "D" },
-                "D" : { " " : "D" }
-            }
-        },
-    },
-    {
-        "boolean" : "#BOOLEAN",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-            "grammar" : ['b', 'o', 'l', 'e', 'a', 'n'],
-            "rules" : {
-                "A" : { "b" : "B" },
-                "B" : { "o" : "C" },
-                "C" : { "o" : "D" },
-                "D" : { "l" : "E" },
-                "E" : { "e" : "F" },
-                "F" : { "a" : "G" },
-                "G" : { "n" : "H" },
-                "H" : { " " : "H" }
-            }
-        },
-    },
+    def find(self, id):
+        if id in self.table:
+            return self.table[id]
+        else:
+            return None
+    
+    def set_attribute(self, attr_name, attr_value, id):
+        if id in self.table:
+            row = self.table[id]
+            row.__dict__[attr_name] = attr_value
+            return self.table[id]
+    
+    def get_attribute(self, attr_name, id):
+        if id in self.table:
+            row = self.table[id]
+            return row[attr_name]
+    
+    def toString(self):
+        for id in self.table:
+            print('key : ', id, 'value : ', self.table[id].toString())
 
-    {
-        "if" : "#IF",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C'],
-            "grammar" : ['i', 'f'],
-            "rules" : {
-                "A" : { "i" : "B" },
-                "B" : { "f" : "C" },
-                "C" : { " " : "C" }
-            }
-        },
-    },
-    { 
-        "else" : "#ELSE",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E'],
-            "grammar" : ['e', 'l', 's'],
-            "rules" : {
-                "A" : { "e" : "B" },
-                "B" : { "l" : "C" },
-                "C" : { "s" : "D" },
-                "D" : { "e" : "E" },
-                "E" : { " " : "E" },
-            }
-        },
-    },
-    { 
-        "while" : "#WHILE",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F'],
-            "grammar" : ['w', 'h', 'i', 'l', 'e'],
-            "rules" : {
-                "A" : { "w" : "B" },
-                "B" : { "h" : "C" },
-                "C" : { "i" : "D" },
-                "D" : { "l" : "E" },
-                "E" : { "e" : "F" },
-                "F" : { " " : "F" }
-            }
-        },
-    },
-    {
-        "for" : "#FOR",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D'],
-            "grammar" : ['f', 'o', 'r'],
-            "rules" : {
-                "A" : { "f" : "B" },
-                "B" : { "o" : "C" },
-                "C" : { "r" : "D" },
-                "D" : { " " : "D" }
-            }
-        },
-    },
-    {
-        "return" : "#RETURN",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
-            "grammar" : ['r', 'e', 't', 'u', 'r', 'n'],
-            "rules" : {
-                "A" : { "r" : "B" },
-                "B" : { "e" : "C" },
-                "C" : { "t" : "D" },
-                "D" : { "u" : "E" },
-                "E" : { "r" : "F" },
-                "F" : { "n" : "G" },
-                "G" : { " " : "G" }
-            }
-        },
-    },
-    {
-        "new" : "#NEW",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D'],
-            "grammar" : ['n', 'e', 'w'],
-            "rules" : {
-                "A" : { "n" : "B" },
-                "B" : { "e" : "C" },
-                "C" : { "w" : "D" },
-                "D" : { " " : "D" }
-            }
-        },
-    },
-    {
-        "this" : "#THIS",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E'],
-            "grammar" : ['t', 'h', 'i', 's'],
-            "rules" : {
-                "A" : { "t" : "B" },
-                "B" : { "h" : "C" },
-                "C" : { "i" : "D" },
-                "D" : { "s" : "E" },
-                "E" : { " " : "E" }
-            }
-        },
-    },
-    {
-        "false" : "#FALSE",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E', 'F'],
-            "grammar" : ['f', 'a', 'l', 's', 'e'],
-            "rules" : {
-                "A" : { "f" : "B" },
-                "B" : { "a" : "C" },
-                "C" : { "l" : "D" },
-                "D" : { "s" : "E" },
-                "E" : { "e" : "F" },
-                "F" : { " " : "F" }
-            }
-        },
-    },
-    {
-        "true" : "#TRUE",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E'],
-            "grammar" : ['t', 'r', 'u', 'e'],
-            "rules" : {
-                "A" : { "t" : "B" },
-                "B" : { "r" : "C" },
-                "C" : { "u" : "D" },
-                "D" : { "e" : "E" },
-                "E" : { " " : "E" }
-            }
-        },
-    },
-    {
-        "null" : "#NULL",
-        "AFD" : { 
-            "states" : ['A', 'B', 'C', 'D', 'E'],
-            "grammar" : ['n', 'u', 'l'],
-            "rules" : {
-                "A" : { "n" : "B" },
-                "B" : { "u" : "C" },
-                "C" : { "l" : "D" },
-                "D" : { "l" : "E" },
-                "E" : { " " : "E" }
-            }
-        },
-    },
 
-    {
-        "identifier" : "#IDENTIFIER",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'w', '_'],
-            "rules" : {
-                "A" : { 
-                    "abcdefghijklmnopqrstuvwxyz" : "B",
-                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "B",
-                    "_" : "B",
-                    " " : "B"
-                },
-                "B" : { 
-                   "abcdefghijklmnopqrstuvwxyz" : "B",
-                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "B",
-                    "_" : "B",
-                    " " : "B"
-                },
-            }
-        },
-    },
-    { 
-        "number" : "#NUMBER",
-        "AFD" : { 
-            "states" : ['A', 'B'],
-            "grammar" : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            "rules" : {
-                "A" : { 
-                    "0123456789" : "B"
-                },
-                "B" : { 
-                    "0123456789" : "B",
-                    " " : "B"
-                },
-            }
-        },
-    }
-]
+class SymbleTableRow:
+    def __init__(self, label, token_type, scope, data_type):
+        self.id = str("%032x" % random.getrandbits(128))
+        self.label = label
+        self.token_typen = token_type
+        self.scope = scope
+        self.data_type = data_type
+    
+    def toString(self):
+        return self.label + ' - ' + self.token_typen + ' - ' + self.scope + ' - ' + self.data_type
